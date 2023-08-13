9e6c602699dcd452669a6f5d4ee156c14b72a1b5
arn <arnaud@chevreton.com>
2023-08-13 04:12:50 +0200

    -

--- ldap/AuditParser.java
+++ ldap/AuditParser.java
@@ -35,24 +35,58 @@ public class AuditParser {
         fileChannel = (FileChannel) Files.newByteChannel(pathToRead, EnumSet.of(StandardOpenOption.READ));
 
 
-        System.out.println("fileChannel: size" + fileChannel.size());
-        System.out.println("fileChannel: isOpen" + fileChannel.isOpen());
-        System.out.println("fileChannel: position" + fileChannel.position());
+        System.out.println("fileChannel: size: " + fileChannel.size());
+        System.out.println("fileChannel: isOpen: " + fileChannel.isOpen());
+        System.out.println("fileChannel: position: " + fileChannel.position());
 
 
         MappedByteBuffer mappedByteBuffer = fileChannel
                 .map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
 
 
-        System.out.println("mappedByteBuffer: loaded:" + mappedByteBuffer.isLoaded());  //prints false
-        System.out.println("mappedByteBuffer cap:" + mappedByteBuffer.capacity());  //Get the size based on content size of file
-        System.out.println("mappedByteBuffer direct:" + mappedByteBuffer.isDirect());
-        System.out.println("mappedByteBuffer ro:" + mappedByteBuffer.isReadOnly());
-        System.out.println("mappedByteBuffer limit:" + mappedByteBuffer.limit());
+        System.out.println("mappedByteBuffer: isLoaded: " + mappedByteBuffer.isLoaded());  //prints false
+        System.out.println("mappedByteBuffer capacity: " + mappedByteBuffer.capacity());  //Get the size based on content size of file
+        System.out.println("mappedByteBuffer isDirect: " + mappedByteBuffer.isDirect());
+        System.out.println("mappedByteBuffer isReadOnly: " + mappedByteBuffer.isReadOnly());
+        System.out.println("mappedByteBuffer limit: " + mappedByteBuffer.limit());
+
+        System.out.println("mappedByteBuffer hasArray: " + mappedByteBuffer.hasArray() );
+        System.out.println("mappedByteBuffer hasRemain :" +mappedByteBuffer.hasRemaining());
+        System.out.println("mappedByteBuffer position: "+ mappedByteBuffer.position());
+
+        mappedByteBuffer.load() ;
+        mappedByteBuffer.hasArray() ;
+        try {
+            Thread.sleep(150);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+
+        byte[] tab=  new byte[200];
+        System.out.println("tab.length: " + tab.length);
+
+/*
 
         if (mappedByteBuffer != null) {
             charBuffer = Charset.forName("UTF-8").decode(mappedByteBuffer);
+            System.out.println("charBuffer hasArray: "+ charBuffer.hasArray() );
+            System.out.println("charBuffer isReadOnly: "+ charBuffer.isReadOnly());
+            System.out.println("charBuffer length: " + charBuffer.length() );
+
+            char[] tab_cb =  charBuffer.array() ;
+            System.out.println("tab_cb length: " + tab_cb.length);
+
         }
+*/
+        System.out.println("mappedByteBuffer hasRemain :" +mappedByteBuffer.hasRemaining());
+        System.out.println("mappedByteBuffer position: "+ mappedByteBuffer.position());
+        mappedByteBuffer.get(tab) ;
+        System.out.println("mappedByteBuffer hasRemain :" +mappedByteBuffer.hasRemaining());
+        System.out.println("mappedByteBuffer position: "+ mappedByteBuffer.position());
+
+
+
 
         try {
             Thread.sleep(15000);
